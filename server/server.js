4488185cc1f15614ccbe663b9624da2630bf0af2
Eden <eden.vandewatyne@estiam.com>
2026-01-23 12:21:31 +0100

    Ajout du CORS

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 /* API: ExpressJS - Nodemon*/
 
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = 3000
+
+const corsOptions = {
+    origin: [
+        "http://localhost:5237",
+        "http://127.0.0.1:5173",
+    ]
+}
+
+app.use(cors(corsOptions))
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello" })
