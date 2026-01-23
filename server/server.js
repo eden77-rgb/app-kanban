d6dba58160f4f6adab848ef85434d37389cfddf8
Eden <eden.vandewatyne@estiam.com>
2026-01-23 11:55:01 +0100

    Ajout du server node

---
+++ server/server.js
@@ -0,0 +1,13 @@
+/* API: ExpressJS - Nodemon*/
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello" })
+})
+
+app.listen(port, () => {
+    console.log("Serveur démarrer sur : http://localhost:3000")
+})
