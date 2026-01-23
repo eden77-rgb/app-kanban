7997fe4aa18bc37872e4ce934bf44c9a50a852bc
Eden <eden.vandewatyne@estiam.com>
2026-01-23 12:48:41 +0100

    Ajout de la route /api/tasks et de la data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 
 const express = require("express")
 const cors = require("cors")
+const { tasks } = require("./tasks")
+
 const app = express()
 const port = 3000
 
@@ -21,6 +23,10 @@ app.get("/", (req, res) => {
 
 app.get("/health", (req, res) => {
     res.json({ status: 200, ok: true })
+})
+
+app.get("/api/tasks", (req, res) => {
+    res.json({ status: 200, tasks: tasks })
 })
 
 
